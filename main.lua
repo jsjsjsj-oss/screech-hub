60b5cf2e368da31759a1c048a167e23b3718ebe3
jsjsjsj-oss <1023527833@qq.com>
2025-10-29 02:50:14 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+--[[Screech Hub - Main Script]]
+local Config = require(script.Parent.config)
+
+-- Services
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+local Lighting = game:GetService("Lighting")
+local Workspace = game:GetService("Workspace")
+local RunService = game:
